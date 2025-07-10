80f49c5c10f6545a9ff42d8ea23570ef287fbe9b
anastasiyazhurko10 <anastasiya.belanovich10@mail.ru>
2025-07-10 19:48:05 +0300

    Создаем функцию-хелпер для генерации данных от min до max.
    Генерируем случайные числа — например, для выбора случайного предсказания или процента вероятности.

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const newForecast = document.querySelector('.current-forecast h1');
 const percent = document.querySelector('.current-forecast p');
 const forecastList = document.querySelector('.forecasts');
 const forecastItem = document.getElementById('forecast-item');
+
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
